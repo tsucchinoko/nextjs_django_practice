27e9f3ba489e669775ecc34b79a6bf257838b40d
tsucchiiinoko <ccya2211@icloud.com>
2022-02-28 19:19:53 +0900

    Layoutを追加

---
+++ components/Layout.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({children, title = "HP by Nextjs"}) {
+    return (
+        <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm  font-mono">
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <header>
+                <nav className="bg-gray-800 w-screen">
+                    <div className="flex items-center pl-8 h-14">
+                        <div className="flex space-x-4">
+                            <Link href="/">
+                                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                                    Home
+                                </a>
+                            </Link>
+                            {/* hrefとファイル名が対応している！ */}
+                            <Link href="/blog-page">
+                                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                                    Home
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main className="flex flex-1 justify-center items-center flex-col w-screen">
+                {children}
+            </main>
+        </div>
+    )
+}
---
+++ pages/blog-page.js
@@ -0,0 +1,9 @@
+function Blog() {
+  return (
+    <div>
+      Blog page
+    </div>
+  )
+}
+
+export default Blog
